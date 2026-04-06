21b70c2d1d1f46e2671e52f6bd9ca4753408597b
i321i <i321ixd@gmail.com>
2026-04-06 10:58:11 +0000

    style: let tailwind's md = 850px

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -11,17 +11,17 @@ export const Navigation = ({ lng }: { lng: string }) => {
   return (
     <nav className="flex h-24 w-screen flex-row flex-wrap justify-between border-b-2 border-solid p-3 pr-10 pl-8 max-sm:h-20">
       <div className="flex flex-row flex-wrap gap-10">
-        <p className="content-center font-serif text-5xl max-md:text-3xl max-sm:text-3xl">
+        <p className="content-center font-serif text-5xl max-md:text-3xl">
           {t("title")}
         </p>
         <Link
           href="/product"
-          className="font-sans-serif content-center text-3xl decoration-2 underline-offset-3 hover:underline active:not-aria-[haspopup]:translate-y-px max-sm:hidden"
+          className="font-sans-serif content-center text-3xl decoration-2 underline-offset-3 hover:underline active:not-aria-[haspopup]:translate-y-px max-md:hidden"
         >
           {t("products")}
         </Link>
       </div>
-      <div className="flex h-full flex-row flex-wrap items-center gap-10 max-md:hidden max-sm:hidden">
+      <div className="flex h-full flex-row flex-wrap items-center gap-10 max-md:hidden">
         <LanguagesSelect lng={lng} />
         <Button
           variant="link"
