eb6e2791f1385ddca251a2ff555e9ade53e53f78
i321i <i321ixd@gmail.com>
2026-05-09 08:38:56 +0000

    feat: init authjs resend

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,12 @@
 import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import Resend from "next-auth/providers/resend";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
 import { cert } from "firebase-admin/app";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [Github, Google],
+  providers: [Github, Google, Resend],
   adapter: FirestoreAdapter({
     credential: cert({
       projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
