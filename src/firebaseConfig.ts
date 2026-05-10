38af9f14a14fede849d618b2048e9d56964ce4e3
i321i <i321ixd@gmail.com>
2026-05-10 08:30:52 +0000

    chore: remove client firebase and set backend firebase

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,6 @@
+import { getApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+
+const app = getApp();
+export const db = getFirestore(app);
+console.log(db);
