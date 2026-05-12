22ef8350cbbecd8090b3a3f76b8fe4becb226ec0
i321i <i321ixd@gmail.com>
2026-05-12 14:47:29 +0000

    feat: finish getstock api

--- src/app/api/products/[productId]/route.ts
+++ src/app/api/products/[productId]/route.ts
@@ -1,4 +1,37 @@
-import products from "@/i18n/locales/en/products.json";
-const dataIds = Object.keys(products);
-export async function GET() {}
-export async function POST() {}
+import { ProductItem } from "@/interfaces/ProductsRepository";
+import { database } from "@/lib/firestore-adapter-instance";
+export async function GET({
+  params,
+}: {
+  params: Promise<{ productId: string }>;
+}) {
+  const { productId } = await params;
+  await database
+    .getStock(productId)
+    .then((result) => result)
+    .catch((reason) => {
+      const message =
+        reason instanceof Error ? reason.message : "getStock failed";
+      return new Response(message, { status: 400 });
+    });
+}
+export async function PATCH(
+  request: Request,
+  {
+    params,
+  }: {
+    params: Promise<{ productId: string }>;
+  },
+) {
+  const { productId } = await params;
+  const body: { stock: ProductItem["stock"] } = await request.json();
+  if (!body.stock) return new Response("body content error", { status: 400 });
+  await database
+    .updateStock(productId, body)
+    .then((result) => result)
+    .catch((reason) => {
+      const message =
+        reason instanceof Error ? reason.message : "updataStock failed";
+      return new Response(message, { status: 400 });
+    });
+}
