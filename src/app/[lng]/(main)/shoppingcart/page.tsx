3733ac74e32b1b1eb2a6e03f9091a7313e4834de
i321i <i321ixd@gmail.com>
2026-05-17 04:59:12 +0000

    style: adjust UI of the tabs of cart page

--- src/app/[lng]/(main)/shoppingcart/page.tsx
+++ src/app/[lng]/(main)/shoppingcart/page.tsx
@@ -13,32 +13,47 @@ export default function Page() {
     if (tab === "information") setActiveTab("confirmation");
   };
   return (
-    <div className="container-1920 flex flex-col flex-wrap content-center justify-center gap-8">
-      <Tabs value={activeTab} className="flex h-auto gap-7">
-        <TabsList variant={null} className={cn("pointer-events-none")}>
+    <div className="container-1920 flex max-w-410 flex-col flex-wrap content-center justify-center gap-8">
+      <Tabs value={activeTab} className="flex w-full gap-8">
+        <TabsList
+          variant={null}
+          className={cn(
+            "pointer-events-none flex w-full flex-row justify-between px-10 group-data-[orientation=horizontal]/tabs:h-14",
+          )}
+        >
           <TabsTrigger
             value="cart"
             className={cn(
-              "flex h-auto flex-col text-lg",
-              `before:flex before:h-6 before:text-sm before:w-6 before:flex-wrap before:content-center before:justify-center before:rounded-4xl before:border-0 before:bg-gray-500 before:text-white before:content-["1"] data-active:before:bg-gray-800`,
+              "flex h-auto max-w-20 flex-col text-lg text-gray-400 data-active:text-gray-600 font-light",
+              `before:flex before:h-6 before:w-6 before:flex-wrap before:content-center before:justify-center before:rounded-4xl before:border-0 before:bg-gray-400 before:text-sm before:text-white before:content-["1"] data-active:before:bg-gray-600`,
             )}
           >
             {t("cart")}
           </TabsTrigger>
+          <div
+            className={cn(
+              "flex h-0 grow -translate-y-0.5 flex-wrap self-center border-b border-solid border-gray-300",
+            )}
+          />
           <TabsTrigger
             value="information"
             className={cn(
-              "flex h-auto flex-col text-lg",
-              `before:flex before:h-6 before:text-sm before:w-6 before:flex-wrap before:content-center before:justify-center before:rounded-4xl before:border-0 before:bg-gray-500 before:text-white before:content-["2"] data-active:before:bg-gray-800`,
+              "flex h-auto max-w-20 flex-col text-lg text-gray-400 data-active:text-gray-600 font-light",
+              `before:flex before:h-6 before:w-6 before:flex-wrap before:content-center before:justify-center before:rounded-4xl before:border-0 before:bg-gray-400 before:text-sm before:text-white before:content-["2"] data-active:before:bg-gray-600`,
             )}
           >
             {t("information")}
           </TabsTrigger>
+          <div
+            className={cn(
+              "flex h-0 grow -translate-y-0.5 flex-wrap self-center border-b border-solid border-gray-300",
+            )}
+          />
           <TabsTrigger
             value="confirmation"
             className={cn(
-              "flex h-auto flex-col text-lg",
-              `before:flex before:h-6 before:text-sm before:w-6 before:flex-wrap before:content-center before:justify-center before:rounded-4xl before:border-0 before:bg-gray-500 before:text-white before:content-["3"] data-active:before:bg-gray-800`,
+              "flex h-auto max-w-20 flex-col text-lg text-gray-400 data-active:text-gray-600 font-light",
+              `before:flex before:h-6 before:w-6 before:flex-wrap before:content-center before:justify-center before:rounded-4xl before:border-0 before:bg-gray-400 before:text-sm before:text-white before:content-["3"] data-active:before:bg-gray-600`,
             )}
           >
             {t("confirmation")}
@@ -49,7 +64,7 @@ export default function Page() {
         <TabsContent value="confirmation">3</TabsContent>
       </Tabs>
       <Button
-        className="flex h-10 cursor-pointer rounded-none bg-gray-600 text-lg font-normal"
+        className="flex h-10 cursor-pointer rounded-none bg-gray-600 text-lg font-normal w-20 m-auto"
         onClick={() => handleSwitchTab(activeTab)}
       >
         next
