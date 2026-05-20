90de3bdfbb12cc405a67a86385e360a969784123
i321i <i321ixd@gmail.com>
2026-05-20 14:08:44 +0000

    fix: fix unexpected displayh of the image of home page

--- src/app/[lng]/(main)/page.tsx
+++ src/app/[lng]/(main)/page.tsx
@@ -17,28 +17,20 @@ export default function Home() {
   // const { lng } = await params;
   // const { t } = await getT("home", { lng });
   return (
-    <div className="container-1920 flex w-screen flex-col gap-8">
+    <div className="container-1920 flex w-screen flex-col gap-8 ">
       {/*墊absolute下面空間*/}
-      <Image
-        src="/images/home.jpg"
-        width={1920}
-        height={100}
-        sizes="100vw"
-        alt="image of home page"
-        loading="eager"
-        priority
-        className="relative left-0 mt-[-2] w-screen object-cover"
-      />
-      <Image
-        src="/images/home.jpg"
-        width={1920}
-        height={100}
-        sizes="100vw"
-        alt="image of home page"
-        loading="eager"
-        priority
-        className="absolute left-0 z-10 mt-[-2] w-screen object-cover"
-      />
+      <div className="h-[46vw] ">
+        <Image
+          src="/images/home.jpg"
+          width={1920}
+          height={100}
+          sizes="100vw"
+          alt="image of home page"
+          loading="eager"
+          priority
+          className="absolute left-0 z-10 mt-[-2] w-screen object-cover "
+        />
+      </div>
       <section className="flex flex-col gap-8">
         <h2 className="h2 pr-12 pl-8">{t("categories")}</h2>
         <div className="nowrap introCardScrollbar flex snap-x scroll-px-12 flex-row justify-start gap-8 overflow-x-scroll pr-12 pb-3 pl-8">
