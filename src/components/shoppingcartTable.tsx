8bc8f85e10bf261ea8e6668541e05040255aee2a
i321i <i321ixd@gmail.com>
2026-05-23 04:00:16 +0000

    feat: finish total on cart page

--- src/components/shoppingcartTable.tsx
+++ src/components/shoppingcartTable.tsx
@@ -35,7 +35,7 @@ export function ShoppingcartTable({ session }: { session: Session }) {
       setCart(data);
     };
     callCartApi();
-  }, [remove]);
+  }, [remove, userId]);
   const { t } = useT("shoppingCart");
   const { t: tProducts } = useT("products");
   const convertNumWithNtAndComma = (num: string) => {
@@ -53,58 +53,64 @@ export function ShoppingcartTable({ session }: { session: Session }) {
       const localInformation = tProducts(item.id, {
         returnObjects: true,
       }) as ProductObject;
+      const subtotal =
+        convertNumWithNtAndComma(localInformation.price) * item.quantity;
       return {
         ...item,
         ...localInformation,
-        subtotal:
-          convertNumWithNtAndComma(localInformation.price) * item.quantity,
+        subtotal,
       };
     });
     return data;
   };
   const data = mergeProductsInformation(cart);
+  const total = data.reduce((sum, item) => sum + item.subtotal, 0);
   return (
-    <div>
-      <div className=" rounded-md border">
-        <Table>
-          <TableHeader>
-            <TableRow className="hover:bg-none">
-              <TableHead className="text-center">{t("image")}</TableHead>
-              <TableHead className="text-center">{t("product")}</TableHead>
-              <TableHead className="text-center">{t("price")}</TableHead>
-              <TableHead className="text-center">{t("quantity")}</TableHead>
-              <TableHead className="text-center">{t("subtotal")}</TableHead>
-              <TableHead className="text-center">{t("remove")}</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {data.map((item) => (
-              <TableRow key={item.id}>
-                <TableCell className="text-center">{item.id}</TableCell>
-                <TableCell className="text-center">{item.title}</TableCell>
-                <TableCell className="text-center">{item.price}</TableCell>
-                <TableCell className="text-center">{item.quantity}</TableCell>
-                <TableCell className="text-center">
-                  NT$ {item.subtotal.toLocaleString()}
-                </TableCell>
-                <TableCell className="flex justify-center">
-                  <Trash2
-                    className="cursor-pointer"
-                    onClick={async () => {
-                      await fetch(
-                        `/api/users/${session.user?.id}/cart/${item.id}`,
-                        { method: "DELETE" },
-                      );
-                      setRemove(remove + 1);
-                    }}
-                  />
-                </TableCell>
+    cart.length > 0 && (
+      <div className="flex flex-col justify-center gap-4">
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              <TableRow className="hover:bg-none">
+                <TableHead className="text-center">{t("image")}</TableHead>
+                <TableHead className="text-center">{t("product")}</TableHead>
+                <TableHead className="text-center">{t("price")}</TableHead>
+                <TableHead className="text-center">{t("quantity")}</TableHead>
+                <TableHead className="text-center">{t("subtotal")}</TableHead>
+                <TableHead className="text-center">{t("remove")}</TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHeader>
+            <TableBody>
+              {data.map((item) => (
+                <TableRow key={item.id}>
+                  <TableCell className="text-center">{item.id}</TableCell>
+                  <TableCell className="text-center">{item.title}</TableCell>
+                  <TableCell className="text-center">{item.price}</TableCell>
+                  <TableCell className="text-center">{item.quantity}</TableCell>
+                  <TableCell className="text-center">
+                    NT$ {item.subtotal.toLocaleString()}
+                  </TableCell>
+                  <TableCell className="flex justify-center">
+                    <Trash2
+                      className="cursor-pointer"
+                      onClick={async () => {
+                        await fetch(
+                          `/api/users/${session.user?.id}/cart/${item.id}`,
+                          { method: "DELETE" },
+                        );
+                        setRemove(remove + 1);
+                      }}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+        <p className="m-auto w-fit text-lg text-gray-500">
+          {`${t("total")}NT$ ${total.toLocaleString()}`}
+        </p>
       </div>
-      {t("total")}
-    </div>
+    )
   );
 }
