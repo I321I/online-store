5d1559ecbd9174e5957de63523298126c1da99b3
i321i <i321ixd@gmail.com>
2026-06-14 13:54:41 +0000

    style: try to set pointer

--- src/app/[lng]/(main)/payment/page.tsx
+++ src/app/[lng]/(main)/payment/page.tsx
@@ -11,23 +11,20 @@ export default async function Page({
   const { lng } = await params;
   const { t } = await getT("common", { lng });
   return (
-    <div className="container-1920 flex max-w-[800px] flex-col gap-10 px-8 min-h-[69.6vh]">
+    <div className="container-1920 flex min-h-[69.6vh] max-w-[800px] flex-col gap-10 px-8">
       <h2 className="flex justify-center text-3xl text-gray-700">
         {t("payment")}
       </h2>
-      <p className="px-2 text-lg font-light text-gray-800 whitespace-pre-wrap">
+      <p className="px-2 text-lg font-light whitespace-pre-wrap text-gray-800">
         {t("paymentContent")}
       </p>
-       <div className="relative h-80">
       <Image
         src={paymentImage}
         alt="payment image"
         placeholder="blur"
         sizes="100vw"
-        fill
-        className="object-cover"
-        />
-        </div>
+        className="pointer-events-none h-80 object-cover"
+      />
     </div>
   );
 }
