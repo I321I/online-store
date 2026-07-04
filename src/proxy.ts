bceaa5d8181c6928ba0f7b6de38525d6199c062e
i321i <i321ixd@gmail.com>
2026-07-04 05:44:47 +0000

    feat: set the proxy cache-control

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,11 +2,26 @@ import { createProxy } from "next-i18next/proxy";
 import i18nConfig from "../i18n.config";
 import { auth } from "@/auth";
 import { NextRequest } from "next/server";
+
 const i18nProxy = createProxy(i18nConfig);
 export default auth((req) => {
   const request = new NextRequest(req);
   request.headers.set("x-current-path", request.nextUrl.pathname);
-  return i18nProxy(request);
+
+  const response = i18nProxy(request);
+  if (!req.auth) {
+    response.headers.set(
+      "Cache-Control",
+      "public, s-maxage=600, stale-while-revalidate=3600",
+    );
+  } else {
+    response.headers.set(
+      "Cache-Control",
+      "no-store, no-cache, must-revalidate, proxy-revalidate",
+    );
+  }
+
+  return response;
 });
 
 export const config = {
